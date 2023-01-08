7b21b6c00a1e1055ee27fd922c6beba1fe4a0950
Codrin Leonte <leontec@ipslazure.onmicrosoft.com>
2023-01-08 10:00:27 +0200

    refactor

--- Homeworks copy/Homework W5 OOP advanced/Exercise 7/Client.cs
+++ Homeworks copy/Homework W5 OOP advanced/Exercise 7/Client.cs
@@ -18,7 +18,7 @@ namespace Homework_W5_OOP_advanced
 			LastName = lastName;
             Phone = phone;
 
-            if ( email.Contains("@") || email.Length < 4 || email.Contains("."))
+            if (!email.Contains("@") || email.Length < 4 || !email.Contains("."))
 			{
 				throw new InvalidEmail("Please note that the mail inserted is wrong formatted");
 			}
